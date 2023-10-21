a7dddc6f1d206e3682f941816dab25a4bce2f375
oldcatY <35653686+imbalaomao@users.noreply.github.com>
2023-10-21 11:57:39 +0800

    Create fuckit_yum.sh

---
+++ fuckit_yum.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo 将开消耗您的流量，此脚本占用宽带。
+echo 将与8秒后开始运行
+sleep 8
+echo 正在检测环境
+yum install screen -y
+echo starting...
+echo 若要停止,重启服务器即可
+sleep 3
+screen_name=$"run"  
+screen -dmS $screen_name  
+cmd=$"while true ;do wget -O /dev/null 'https://speed.cloudflare.com/__down?during=download&bytes=107374182400'; done;";  
+screen -x -S $screen_name -p 0 -X stuff "$cmd"  
+screen -x -S $screen_name -p 0 -X stuff $'\n'  
+echo  请时刻留意此脚本更新 https://www.diannaobos.com/post/921.html
